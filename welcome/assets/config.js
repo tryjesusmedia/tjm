cdf7a6f96544a797aa31fad088929093ca2b1415
tryjesusmedia <tryjesusnewyork@gmail.com>
2026-07-27 21:41:03 -0500

    Update config.js

--- welcome/assets/config.js
+++ welcome/assets/config.js
@@ -5,12 +5,12 @@
   such as: https://us06web.zoom.us/j/123456789?pwd=...
 */
 window.TJM_CONFIG = {
-  zoomUrl: 'https://zoombiblestudy.com/',
+  zoomUrl: 'https://us06web.zoom.us/j/4700414908',
   discussionDay: 4,
   discussionHourEastern: 20,
   discussionMinuteEastern: 0,
   social: {
-    youtube: 'https://www.youtube.com/@KalmanRoller',
+    youtube: 'https://www.youtube.com/@TryJesusMedia',
     facebook: 'https://www.facebook.com/profile.php?id=61575629752301',
     instagram: 'https://www.instagram.com/zoom.bible.study?igsh=MWFnOHUzMnZrbWt6Nw%3D%3D',
     tiktok: 'https://tiktok.com/@zoombiblestudy.com',
