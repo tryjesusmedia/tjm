882ee7e20dbc4088979c98627498b5571a9631ef
tryjesusmedia <tryjesusnewyork@gmail.com>
2026-09-09 20:38:21 -0500

    Load reading-task principle name controls

--- chronbible/config.js
+++ chronbible/config.js
@@ -12,6 +12,8 @@ window.TJM_CHRONBIBLE_CONFIG = Object.freeze({
 document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@xyflow/react@12.11.3/dist/style.css">');
 document.write('<link rel="stylesheet" href="../lib/principles-folders-flow.css?v=20260904-1">');
 document.write('<link rel="stylesheet" href="../lib/principles-text-size.css?v=20260908-1">');
+document.write('<link rel="stylesheet" href="../lib/reading-principle-names.css?v=20260909-1">');
 document.write('<script defer src="../lib/principles-react-flow-bridge.js?v=20260903-2"><\/script>');
+document.write('<script defer src="../lib/reading-principle-names.js?v=20260909-1"><\/script>');
 document.write('<script type="module" src="../lib/principles-folders-flow.mjs?v=20260904-3"><\/script>');
 document.write('<script defer src="../lib/principles-text-size.js?v=20260908-1"><\/script>');
